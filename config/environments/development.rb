221035395a501bbe37c11fff2d4c2c902d423485
Dawna Ara <dawnaara@gmail.com>
2016-12-03 16:01:26 -0800

    updated home page image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+ 
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -31,10 +27,11 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  Paperclip.options[:command_path] = "/usr/local/bin/convert"
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
