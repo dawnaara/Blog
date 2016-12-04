0c7d2be93cd2ba00d974454683eea83e154b2584
Dawna Ara <dawnaara@gmail.com>
2016-12-04 08:16:58 -0800

    added blog index and show pages from bootstrap templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,22 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
+    #add will_paginate
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
-    @post = Post.order("created_at DESC").limit(4).offset(1)
+    @post = Post.find(params[:id])#.order("created_at DESC").limit(4).offset(1)
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
 
@@ -38,8 +31,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -52,8 +43,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -63,12 +52,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body, :user_id, :image)
     end
